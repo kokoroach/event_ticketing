98e6a634e4531d8c9b8062ee080d7faf9fcddbfe
kokoroach <jonel.getigan@gmail.com>
2025-11-27 18:21:43 +0800

    refactor: improve routes def, add healthcheck

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,15 +1,10 @@
-from functools import partial
-
 from fastapi import APIRouter
 
 from app.api.v1.routers.event_router import router as event_router
+from app.api.v1.routers.health_router import router as health_router
 
 
-def _api_prefix(version: str, resource: str) -> str:
-    return f"/api/{version}/{resource}"
+api_v1_router = APIRouter(prefix="/api/v1", tags=["API v1"])
 
-
-api_v1 = partial(_api_prefix, "v1")
-
-api_v1_router = APIRouter()
-api_v1_router.include_router(event_router, prefix=api_v1("events"), tags=["Events"])
+api_v1_router.include_router(health_router, prefix="/health-check", tags=["Health"])
+api_v1_router.include_router(event_router, prefix="/events", tags=["Events"])
